2c2a3a8911b71e1d94d7aac90414f9e6e051a1ca
HANNIBO <66998508+hannibo3112@users.noreply.github.com>
2020-07-28 12:36:32 +0630

    Update SpeakGoodbye.js

--- mod4_solution/SpeakGoodbye.js
+++ mod4_solution/SpeakGoodbye.js
@@ -3,6 +3,7 @@
 
 // STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
 // See Lecture 52, part 2
+var byeSpeaker={}:
 
 
 // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
