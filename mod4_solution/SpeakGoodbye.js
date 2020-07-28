476003800e486f85f012d1bfd314a5add20f9ab7
HANNIBO <66998508+hannibo3112@users.noreply.github.com>
2020-07-28 13:19:19 +0630

    Update SpeakGoodbye.js

--- mod4_solution/SpeakGoodbye.js
+++ mod4_solution/SpeakGoodbye.js
@@ -6,7 +6,7 @@
 
 (function (window) {
 
-
+var speak = function(){}
 // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
